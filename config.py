83fe25fb3485239c511c0a6f63d700c0d2e3a9a8
Mark <robinson.mark.charles@gmail.com>
2024-10-16 21:34:05 +1100

    Clearer documentation of config file

--- config.py
+++ config.py
@@ -5,13 +5,25 @@ Created on Thu Sep 19 19:12:52 2024
 @author: robinsonmc
 """
 
+#Set this to 1 for verbose messages while running the code
 GBL_DEBUG = 0
+
+#Path to the downloaded data:
 GBL_PATH_TO_DATA = "D:\Data_for_up"
-GBL_PATH_TO_SAVED_FEATURES = "D:\Data\saved_features"
+
+#Path to a set of features extracted for previous analysis that can be loaded
+#if the segment_and_extract_features flag is set to false.
+GBL_PATH_TO_SAVED_FEATURES = "./new_saved_features_11_subjects"
+
+#Binary file containing information linking filenames to subjects and runs
 GBL_PATH_TO_METADATA_ORIG = 'metadata_all_shearers.pickle'
 
-#This directory needs to exist
+#Path where the extracted features are saved if the 
+# segment_and_extract_features flag is set to false. NOTE THAT THIS
+# DIRECTORY MUST ALREADY EXIST, it will not be created by the code.
 GBL_EXTRACTED_FEATURES_SAVE_PATH = "saved_features_test_extract"
 
-#This file will be generated if it does not already exist
+#If the extract_metadata flag is set to true, the metadata will be saved
+#at the following path. This file will be generated if it does not 
+#already exist, and it will overwrite if it does exist.
 GBL_SAVEPATH_FOR_GENERATED_METADATA = 'metadata_all_shearers_generated.pickle'
